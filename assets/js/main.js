e7a83f4c86bff6daea8f4921fca3b9106fca5240
Mahammad <mahammad.c@code.edu.az>
2017-09-07 15:34:55 +0500

    moorget

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,13 +16,13 @@ $(document).ready(function(){
 		}
   });
 });
-// $(function(){
-//   $.scrollIt();
-// });
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
+$(function(){
+  $.scrollIt();
 });
+// $(document).on('click', 'a', function(event){
+//     event.preventDefault();
+//
+//     $('html, body').animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 500);
+// });
